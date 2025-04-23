7d27bb6ac5c0b788f13e3ed6b5635a8426754cc0
ggaappa <przewlockifilip90@gmail.com>
2025-04-23 23:47:21 +0200

    import

---
+++ server.js
@@ -0,0 +1,17 @@
+// Prześmiewcza strona o pogrzebie chomika Alberta
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serwuj pliki statyczne z folderu public
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Serwer działa na http://localhost:${PORT}`);
+});
